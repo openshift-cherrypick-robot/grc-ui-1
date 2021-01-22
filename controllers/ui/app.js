ef4775b2d1ae9ca74c3ce4b2cd1a616023b16f62
Will <willkutler@gmail.com>
2021-01-22 14:08:53 -0700

    clean up useraccess stuff

--- controllers/ui/app.js
+++ controllers/ui/app.js
@@ -21,11 +21,7 @@ const ReactDOMServer = require('react-dom/server'),
       appUtil = require('../../lib/server/app-util'),
       Provider = require('react-redux').Provider,
       router = express.Router({ mergeParams: true }),
-      // accessCtrl = require('../useraccess'),
-      request = require('../../lib/server/request'),
-      // async = require('async'),
-      log4js = require('log4js'),
-      logger = log4js.getLogger('namespace-client')
+      request = require('../../lib/server/request')
 
 import _ from 'lodash'
 
@@ -36,9 +32,7 @@ const targetAPIGroups = [
   'apps.open-cluster-management.io',
 ]
 
-// auth = req.headers.Authorization || req.headers.authorization || `Bearer ${cookieUtil.getAccessToken(req)
 const getUserNamespaces = (auth, cb) => {
-  console.log('getUserNS')
   const options = {
     url: `${config.get('API_SERVER_URL')}/apis/project.openshift.io/v1/projects`,
     headers: {
@@ -59,11 +53,10 @@ const getUserNamespaces = (auth, cb) => {
       ? userNamespaces.map(item => ({ Name: item.metadata.name }))
       : []
     cb(err, userNamespaces)
-  }, logger)
+  })
 }
 
 function nsFormatter(multiNS) {
-  console.log('ns formatter')
   let formattedNS = []
   if (Array.isArray(multiNS) && multiNS.length > 0) {
     formattedNS = multiNS.map((ns) => {
@@ -73,46 +66,10 @@ function nsFormatter(multiNS) {
         return ns.name
       }})
   }
-  // logger.info(`formattedNS is : ${JSON.stringify(formattedNS)}`)
   return formattedNS
 }
 
-// get user access info on single namespace
-// async function getUserAccess(req, cb){
-//   console.log('user access')
-//   const k8sAPI = '/apis/authorization.k8s.io/v1'
-//   const options = {
-//     method: 'POST',
-//     url: `${config.get('API_SERVER_URL')}${k8sAPI}/selfsubjectrulesreviews`,
-//     json: true,
-//     headers: {
-//       'Content-Type': 'application/json',
-//       'Accept': 'application/json',
-//       Authorization: req.authorizationToken
-//     },
-//     body: {
-//       apiVersion: 'authorization.k8s.io/v1',
-//       kind: 'SelfSubjectRulesReview',
-//       spec: {
-//         namespace: req.singleNS
-//       },
-//     }
-//   }
-
-//   request(options, null, [200,201], (err, res) => {
-//     if (err) {
-//       return cb(err, null)
-//     }
-//     console.log('user access cb')
-//     const userAccess = userAccessFormatter(req, res)
-//     console.log('got userAccess, putting into cb')
-//     console.log(userAccess)
-//     return cb(null, userAccess)
-//   }, logger)
-// }
-
 async function getUserAccess(authorizationToken, singleNS, apiGroups, rawDataFlag, resolve, reject){
-  console.log('user access')
   const k8sAPI = '/apis/authorization.k8s.io/v1'
   const options = {
     method: 'POST',
@@ -136,44 +93,13 @@ async function getUserAccess(authorizationToken, singleNS, apiGroups, rawDataFla
     if (err) {
       return reject(err)
     }
-    console.log('user access cb')
     const userAccess = userAccessFormatter(res, apiGroups, singleNS, rawDataFlag)
-    console.log('got userAccess, putting into cb')
-    console.log(userAccess)
     return resolve(userAccess)
-  }, logger)
+  })
 }
 
-// parallelly calling to get user access info on each NS
-// by using user namespaces passed from getHeader/getHeaderData
-// const getUserAccessInfo = (authorizationToken, targetAPIGroups, rawDataFlag, namespaces, cb) => {
-//   console.log('GET UA INFO')
-//   const userAllNS = nsFormatter(namespaces)
-//   // logger.info(`user all NS got from getHeader/getHeaderData are : ${JSON.stringify(userAllNS)}`)
-//   const userAccessReq = []
-//   userAllNS.map((singleNS) => {// each element binds with one NS then parallelly call whole array
-//     userAccessReq.push(
-//       getUserAccess.bind({
-//         authorizationToken,
-//         singleNS,
-//         targetAPIGroups,
-//         rawDataFlag
-//       }))
-//   })
-//   async.parallel(userAccessReq, (err, userAccessResult) => {
-//     console.log('--- getUserAccessInfo callback -----')
-//     if (err) {
-//       return cb(err, null)
-//     }
-//     // logger.info(`user access data got from step 2 is : ${JSON.stringify(userAccessResult)}`)
-//     return cb(null, { userAccess: userAccessResult })
-//   })
-// }
-
 const getUserAccessInfo = (authorizationToken, targetAPIGroups, rawDataFlag, namespaces, cb) => {
-  console.log('GET UA INFO')
   const userAllNS = nsFormatter(namespaces)
-  // logger.info(`user all NS got from getHeader/getHeaderData are : ${JSON.stringify(userAllNS)}`)
   const userAccessReq = []
   userAllNS.map((singleNS) => {// each element binds with one NS then parallelly call whole array
     userAccessReq.push(
@@ -184,21 +110,16 @@ const getUserAccessInfo = (authorizationToken, targetAPIGroups, rawDataFlag, nam
   })
   Promise.all(userAccessReq).then(
     (userAccessResult) => {
-      console.log('success!')
-      console.log(userAccessResult)
       return cb(null, userAccessResult)
     }
   ).catch(
     (err) => {
-      console.log('error!')
-      console.log(err)
       return cb(err, null)
     }
   )
 }
 
 function userAccessFormatter(accessInfo, apiGroups, singleNS, rawDataFlag) {
-  console.log('userAccessFormatter')
   const targetAPIGroups = new Set(apiGroups)
   const singleNSAccess ={
     namespace: singleNS,
@@ -233,50 +154,8 @@ function userAccessFormatter(accessInfo, apiGroups, singleNS, rawDataFlag) {
       }
     })
   }
-  console.log('breaking out of userAccessFormatter')
   return singleNSAccess
 }
-
-// format/combine/simplify/deal with * for user access raw data on single NS
-// function userAccessFormatter(req, accessInfo) {
-//   console.log('userAccessFormatter')
-//   const targetAPIGroups = new Set(req.targetAPIGroups)
-//   const singleNSAccess ={
-//     namespace: req.singleNS,
-//     rules: {},
-//   }
-//   // if user query raw = true, also return original raw access data on each namespace
-//   if (req.rawDataFlag) {
-//     singleNSAccess.rawData = accessInfo
-//   }
-//   const resourceRules = _.get(accessInfo,'body.status.resourceRules','')
-//   if (Array.isArray(resourceRules) && resourceRules.length > 0) {
-//     resourceRules.forEach((resourceRule) => {
-//       if (Array.isArray(resourceRule.apiGroups)) {
-//         const apiGroups = _.compact(resourceRule.apiGroups)
-//         resourceRule.apiGroups.length > 0 && apiGroups.forEach((api) => {
-//           // no matter if user want *, always return these special cases
-//           if (Array.isArray(resourceRule.resources) && (targetAPIGroups.has(api) || api === '*')) {
-//             const resources = _.compact(resourceRule.resources)
-//             const verbs = _.compact(resourceRule.verbs)
-//             resourceRule.resources.length > 0 && resources.forEach((resource) => {
-//               // use the combined api + resource as the unique key
-//               const mapKey = `${api}/${resource}`
-//               if (Object.prototype.hasOwnProperty.call(singleNSAccess.rules, mapKey)) {
-//                 singleNSAccess.rules[mapKey] = _.union(singleNSAccess.rules[mapKey], verbs)
-//               }
-//               else {
-//                 singleNSAccess.rules[mapKey] = verbs
-//               }
-//             })
-//           }
-//         })
-//       }
-//     })
-//   }
-//   console.log('breaking out of userAccessFormatter')
-//   return singleNSAccess
-// }
 
 router.get('*', (req, res) => {
   reducers = reducers === undefined ? require('../../src-web/reducers') : reducers
@@ -284,17 +163,6 @@ router.get('*', (req, res) => {
   const store = redux.createStore(redux.combineReducers(reducers), redux.applyMiddleware(
     thunkMiddleware, // lets us dispatch() functions
   ))
-  // const optionsUrlQuery = `serviceId=grc-ui&dev=${process.env.NODE_ENV === 'development'}&targetAPIGroups=${JSON.stringify(targetAPIGroups)}`
-  // const options = {
-  //   method: 'GET',
-  //   url: `https://${req.headers.host}/multicloud/access?${optionsUrlQuery}`,
-  //   json: true,
-  //   headers: {
-  //     Cookie: req.headers.cookie,
-  //     Authorization: req.headers.Authorization || req.headers.authorization || `Bearer ${req.cookies['acm-access-token-cookie']}`,
-  //     'Accept-Language': i18n.locale(req)
-  //   }
-  // }
 
   let userAccess
   const auth = req.headers.Authorization || req.headers.authorization || `Bearer ${req.cookies['acm-access-token-cookie']}`
@@ -304,16 +172,11 @@ router.get('*', (req, res) => {
     }
     getUserAccessInfo(auth, targetAPIGroups, null, userNS, (err, ua) => {
       if (err) {
-        console.log('---- get error ------')
-        console.log(err)
         return res.status(err.statusCode || 500).send(err.details)
       }
-      console.log('---- post uA request')
-      console.log(ua)
       userAccess = ua
       access = access === undefined ? require('../../src-web/actions/access') : access
       if (userAccess) {
-        // logger.info(`userAccess is : ${JSON.stringify(userAccess)}`)
         store.dispatch(access.userAccessSuccess(userAccess))
       }
 
@@ -326,29 +189,6 @@ router.get('*', (req, res) => {
       fetchHeader(req, res, store, fetchHeaderContext)
     })
   })
-
-  // request(options, null, [200], (err, results) => {
-  //     if (err) {
-  //       return res.status(err.statusCode || 500).send(err.details)
-  //     }
-  //     console.log('---- post uA request')
-  //     console.log(results)
-  //     userAccess = results.userAccess
-  //     access = access === undefined ? require('../../src-web/actions/access') : access
-  //     if (userAccess) {
-  //       // logger.info(`userAccess is : ${JSON.stringify(userAccess)}`)
-  //       store.dispatch(access.userAccessSuccess(userAccess))
-  //     }
-
-  //     Login = Login === undefined ? require('../../src-web/actions/login') : Login
-  //     store.dispatch(Login.receiveLoginSuccess(req.user))
-
-  //     App = App === undefined ? require('../../src-web/containers/App').default : App
-
-  //     const fetchHeaderContext = getContext(req)
-  //     fetchHeader(req, res, store, fetchHeaderContext)
-  //   }
-  // )
 })
 
 function fetchHeader(req, res, store, ctx) {
