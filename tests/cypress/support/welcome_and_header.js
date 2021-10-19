a6189be5b2a11dacafe16c6fc9728f2760ce2fe7
Kevin Cormier <kcormier@redhat.com>
2021-10-19 12:51:40 -0400

    Only wait after clicking app-dropdown (#821)
    Signed-off-by: Kevin Cormier <kcormier@redhat.com>

--- tests/cypress/support/welcome_and_header.js
+++ tests/cypress/support/welcome_and_header.js
@@ -159,7 +159,7 @@ export const userMenu = {
             return cy.get('[data-test="app-dropdown"]').should('be.visible')
         }, {'interval': 1000, 'timeout':120000})
         // eslint-disable-next-line cypress/no-unnecessary-waiting
-        cy.get('[data-test="app-dropdown"]').should('be.visible').wait(500).click().wait(500)
+        cy.get('[data-test="app-dropdown"]').should('be.visible').click().wait(500)
         // seems the split line is also li
         // tested many times and can't use { force: true } on the above step
         // will not open dropdown after force click and failed on the rest steps
@@ -170,7 +170,7 @@ export const userMenu = {
         cy.contains('ArgoCD').should('be.visible')
         cy.waitUntil(() => cy.get('[data-test="app-dropdown"]').should('be.visible'))
         // eslint-disable-next-line cypress/no-unnecessary-waiting
-        cy.get('[data-test="app-dropdown"]').should('be.visible').wait(500).click().wait(500)
+        cy.get('[data-test="app-dropdown"]').should('be.visible').click().wait(500)
         cy.contains('ArgoCD').should('not.exist')
     },
     openAppsNoArgo: () => {
@@ -182,7 +182,7 @@ export const userMenu = {
             return cy.get('[data-test="app-dropdown"]').should('be.visible')
         }, {'interval': 1000, 'timeout':120000})
         // eslint-disable-next-line cypress/no-unnecessary-waiting
-        cy.get('[data-test="app-dropdown"]').should('be.visible').wait(500).click().wait(500)
+        cy.get('[data-test="app-dropdown"]').should('be.visible').click().wait(500)
         // tested many times and can't use force: true on the above step
         // will not open dropdown after force click and failed on the rest steps
         cy.get('[data-test="app-dropdown"] li').should('be.visible').and('have.length', 2)
