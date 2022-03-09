d57ddc018c3f0c883ba53a01cb13181333d36c62
Yu Cao <ycao@redhat.com>
2022-03-09 13:16:11 -0500

    Fix cluster patch (#870)
    Signed-off-by: Yu Cao <ycao@redhat.com>

--- build/cluster-patch.sh
+++ build/cluster-patch.sh
@@ -11,58 +11,5 @@ while UNFINISHED="$(oc -n ${acm_installed_namespace} get pods | grep -v -e "Comp
   sleep 5
 done
 
-VERSION_TAG=${VERSION_TAG:-"latest"}
-DOCKER_URI="quay.io/stolostron"
-COMPONENT="grc-ui-api"
-if [[ "${RUN_LOCAL}" == "true" ]]; then
-  docker pull ${DOCKER_URI}/${COMPONENT}:${VERSION_TAG}
-  docker run -d -t -i -p 4000:4000 --name grcuiapi -e NODE_ENV=development -e SERVICEACCT_TOKEN=${SERVICEACCT_TOKEN} -e API_SERVER_URL=${API_SERVER_URL} ${DOCKER_URI}/${COMPONENT}:${VERSION_TAG}
-else
-  echo "* Patching hub cluster to ${VERSION_TAG}"
-  oc annotate MultiClusterHub multiclusterhub -n ${acm_installed_namespace} mch-pause=true --overwrite
-  
-  # Patch the API on the hub
-  LABEL="component=ocm-grcuiapi"
-  DEPLOYMENT=$(oc get deployment -l ${LABEL} -n ${acm_installed_namespace} -o=jsonpath='{.items[*].metadata.name}')
-  oc patch deployment ${DEPLOYMENT} -n ${acm_installed_namespace} -p "{\"spec\":{\"template\":{\"spec\":{\"containers\":[{\"name\":\"${COMPONENT}\",\"image\":\"${DOCKER_URI}/${COMPONENT}:${VERSION_TAG}\"}]}}}}"
-  
-  # Patch the propagator on the hub
-  COMPONENT="governance-policy-propagator"
-  LABEL="component=ocm-policy-propagator"
-  DEPLOYMENT=$(oc get deployment -l ${LABEL} -n ${acm_installed_namespace} -o=jsonpath='{.items[*].metadata.name}')
-  oc patch deployment ${DEPLOYMENT} -n ${acm_installed_namespace} -p "{\"spec\":{\"template\":{\"spec\":{\"containers\":[{\"name\":\"${COMPONENT}\",\"image\":\"${DOCKER_URI}/${COMPONENT}:${VERSION_TAG}\"}]}}}}"
-  
-  # Patch managed cluster components
-  echo "* Patching managed clusters to ${VERSION_TAG}"
-  MANAGED_CLUSTERS=$(oc get managedcluster -o=jsonpath='{.items[*].metadata.name}')
-  
-  for MANAGED_CLUSTER in ${MANAGED_CLUSTERS}; do      
-      FOUND="false"
-      while [[ "${FOUND}" == "false" ]]; do
-        echo "* Wait for manifestwork on ${MANAGED_CLUSTER}:"
-        FOUND="true"
-        for COMPONENT in $(ls ${DIR}/patches); do
-          if (! oc get manifestwork -n ${MANAGED_CLUSTER} ${MANAGED_CLUSTER}-klusterlet-addon-${COMPONENT}); then
-            FOUND="false"
-          fi
-        done
-        sleep 5
-      done
-      oc annotate klusterletaddonconfig -n ${MANAGED_CLUSTER} ${MANAGED_CLUSTER} klusterletaddonconfig-pause=true --overwrite=true
-      for COMPONENT in $(ls ${DIR}/patches); do
-        oc patch manifestwork -n ${MANAGED_CLUSTER} ${MANAGED_CLUSTER}-klusterlet-addon-${COMPONENT} --type='json' -p=`cat $DIR/patches/${COMPONENT} | sed 's/:latest/:'${VERSION_TAG}'/g'` || true
-      done
-  done
+./build/patch-dev-images.sh
 
-  echo "* Deleting pods and waiting for restart"
-  oc delete pod -l app=grc -A
-  oc delete pod -l component=governance -A
-  oc delete pod -l app=klusterlet-addon-iampolicyctrl -A
-  oc delete pod -l app=cert-policy-controller -A
-
-  ./build/wait_for.sh pod -l app=grc -A
-  ./build/wait_for.sh pod -l component=governance -A
-  ./build/wait_for.sh pod -l app=klusterlet-addon-iampolicyctrl -A
-  ./build/wait_for.sh pod -l app=cert-policy-controller -A
-
-fi
---
+++ build/patch-dev-images.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+# Copyright Contributors to the Open Cluster Management project
+
+
+set -e
+
+acm_installed_namespace=`oc get subscriptions.operators.coreos.com --all-namespaces | grep advanced-cluster-management | awk '{print $1}'`
+VERSION_TAG=${VERSION_TAG:-"latest"}
+DOCKER_URI="quay.io/stolostron"
+
+echo "* Patching hub cluster to ${VERSION_TAG}"
+oc annotate MultiClusterHub multiclusterhub -n ${acm_installed_namespace} mch-pause=true --overwrite
+
+# Patch the UI on the hub
+COMPONENT="grc-ui"
+LABEL="component=ocm-grcui"
+DEPLOYMENT=$(oc get deployment -l ${LABEL} -n ${acm_installed_namespace} -o=jsonpath='{.items[*].metadata.name}')
+oc patch deployment ${DEPLOYMENT} -n ${acm_installed_namespace} -p "{\"spec\":{\"template\":{\"spec\":{\"containers\":[{\"name\":\"${COMPONENT}\",\"imagePullPolicy\":\"Always\",\"image\":\"${DOCKER_URI}/${COMPONENT}:${VERSION_TAG}\"}]}}}}"
+
+# Patch the API on the hub
+COMPONENT="grc-ui-api"
+LABEL="component=ocm-grcuiapi"
+DEPLOYMENT=$(oc get deployment -l ${LABEL} -n ${acm_installed_namespace} -o=jsonpath='{.items[*].metadata.name}')
+oc patch deployment ${DEPLOYMENT} -n ${acm_installed_namespace} -p "{\"spec\":{\"template\":{\"spec\":{\"containers\":[{\"name\":\"${COMPONENT}\",\"imagePullPolicy\":\"Always\",\"image\":\"${DOCKER_URI}/${COMPONENT}:${VERSION_TAG}\"}]}}}}"
+
+# Patch the propagator on the hub
+COMPONENT="governance-policy-propagator"
+LABEL="component=ocm-policy-propagator"
+DEPLOYMENT=$(oc get deployment -l ${LABEL} -n ${acm_installed_namespace} -o=jsonpath='{.items[*].metadata.name}')
+oc patch deployment ${DEPLOYMENT} -n ${acm_installed_namespace} -p "{\"spec\":{\"template\":{\"spec\":{\"containers\":[{\"name\":\"${COMPONENT}\",\"imagePullPolicy\":\"Always\",\"image\":\"${DOCKER_URI}/${COMPONENT}:${VERSION_TAG}\"}]}}}}"
+
+# Patch the addon-controller on the hub
+COMPONENT="governance-policy-addon-controller"
+LABEL="component=ocm-policy-addon-ctrl"
+DEPLOYMENT=$(oc get deployment -l ${LABEL} -n ${acm_installed_namespace} -o=jsonpath='{.items[*].metadata.name}')
+oc patch deployment ${DEPLOYMENT} -n ${acm_installed_namespace} -p "{\"spec\":{\"template\":{\"spec\":{\"containers\":[{\"name\":\"manager\",\"imagePullPolicy\":\"Always\",\"image\":\"${DOCKER_URI}/${COMPONENT}:${VERSION_TAG}\"}]}}}}"
+
+# Patch the addon-controller envs
+CONTAINERS=(cert-policy-controller config-policy-controller iam-policy-controller governance-policy-spec-sync governance-policy-status-sync governance-policy-template-sync)
+for CONTAINER in ${CONTAINERS[@]}; do
+  IMAGE_NAME=$(echo $CONTAINER | tr 'a-z' 'A-Z' | tr '-' '_')_IMAGE
+  oc set env deployment/${DEPLOYMENT} -n ${acm_installed_namespace} ${IMAGE_NAME}=${DOCKER_URI}/${CONTAINER}:${VERSION_TAG}
+done
+
+# Patch managed cluster components
+echo "* Patching managed clusters to ${VERSION_TAG}"
+MANAGED_CLUSTERS=$(oc get managedcluster -o=jsonpath='{.items[*].metadata.name}')
+
+ADDON_COMPONENTS=(cert-policy-controller config-policy-controller iam-policy-controller governance-policy-framework)
+for MANAGED_CLUSTER in ${MANAGED_CLUSTERS}; do      
+    FOUND="false"
+    while [[ "${FOUND}" == "false" ]]; do
+      echo "* Wait for manifestwork on ${MANAGED_CLUSTER}:"
+      FOUND="true"
+      for COMPONENT in ${ADDON_COMPONENTS[@]}; do
+        if (! oc get manifestwork -n ${MANAGED_CLUSTER} addon-${COMPONENT}-deploy); then
+          FOUND="false"
+        fi
+      done
+      sleep 5
+    done
+    # Patch imagePullPolicy
+    for COMPONENT in ${ADDON_COMPONENTS[@]}; do
+      echo "* Patch imagePullPolicy for ${COMPONENT}"
+      oc annotate ManagedClusterAddOn ${COMPONENT} -n ${MANAGED_CLUSTER} --overwrite addon.open-cluster-management.io/values={\"global\":{\"imagePullPolicy\":\"Always\"}}  
+    done
+done
+
+
+
+echo "* Deleting pods and waiting for restart"
+oc delete pod -l app=grc -A
+oc delete pod -l app=governance-policy-framework -A
+oc delete pod -l app=config-policy-controller -A
+oc delete pod -l app=iam-policy-controller -A
+oc delete pod -l app=cert-policy-controller -A
+
+./build/wait_for.sh pod -l app=grc -A
+./build/wait_for.sh pod -l app=governance-policy-framework -A
+./build/wait_for.sh pod -l app=config-policy-controller -A
+./build/wait_for.sh pod -l app=iam-policy-controller -A
+./build/wait_for.sh pod -l app=cert-policy-controller -A
